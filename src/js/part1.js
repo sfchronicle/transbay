386e3db7b2a71bf18aaf391f0a0acb1c825acc06
emma <emmaruthoneill@gmail.com>
2017-08-25 19:08:12 -0700

    getting happier with this animation

--- src/js/part1.js
+++ src/js/part1.js
@@ -27,89 +27,82 @@ function getPageScroll() {
 var testDiv = document.getElementById("floor-image");
 var testImg = testDiv.getElementsByTagName('img')[0];
 
+var updateImg = function(i,testImg) {
+
+  if (i > 0) {
+    testImg.style.marginLeft = -100*i+ 'px';
+  }
+  if (screen.width <= 2000) {
+    testImg.style.width  = "150%";
+  } else {
+    testImg.style.width  = "200%";
+  }
+}
+
 // fills in HTML for year as years toggle
 var updateInfo = function(data) {
   document.querySelector("#interactive-photo").innerHTML = "<img src="+data.image+"></img>";
   document.querySelector("#interactive-desc").innerHTML = "<div>"+data.text+"</div>";
-
-  testImg.style.marginLeft = -100*i+ 'px';
-  testImg.style.width  = "130%";
 };
 
+// updateInfo(floor1Data[0]);
+console.log(floor1Data);
 
 function activate() {
 
   var sticker = document.getElementById('stick-me');
   var sticker_ph = document.getElementById('stick-ph');
+  var sticker_ph_top = document.getElementById('stick-ph-top');
   var window_top = document.body.scrollTop;
-  var sticker_stop = document.getElementById('stop-stick-here').getBoundingClientRect().top + window_top-offset;
-  var div_top = document.getElementById('stick-here').getBoundingClientRect().top + window_top - 37;
+
+  var sticker_stop = document.getElementById('stop-stick-here').getBoundingClientRect().top + window_top-document.getElementById('stick-me').clientHeight;
+  var div_top = document.getElementById('stick-here').getBoundingClientRect().top + window_top;
 
   if ((window_top > div_top) && (window_top < sticker_stop)) {
     sticker.classList.add('fixed-class');
-    if (screen.width <= 480) {
-      sticker_ph.style.height = "3000px";
-    } else {
-      sticker_ph.style.height = "2500px";
-    }
+    sticker.classList.remove('absolute-class');
+    sticker_ph.style.height = "800px";//document.getElementById('stick-me').clientHeight;
     sticker_ph.style.display = 'block'; // puts in a placeholder for where sticky used to be for smooth scrolling
+    sticker_ph_top.style.display = "none";
   } else {
     sticker.classList.remove('fixed-class');
+    // sticker.classList.add('absolute-class');
+    // sticker_ph.style.height = "0px";
+    // sticker_ph.style.height = "300px";
     sticker_ph.style.display = 'none'; // removes placeholder
+    sticker_ph_top.style.display = 'none';
+
     testImg.style.width  = "100%";
     testImg.style.marginLeft  = "0px";
   }
-
-  var currentPosition = getPageScroll();
-  if (screen.width <= 480) {
-    i = Math.floor(currentPosition/2900*10)-1;
-  } else {
-    i = Math.floor(currentPosition/2500*3)-1;
+  if (window_top >= sticker_stop) {
+    sticker_ph_top.style.height = "800px";//document.getElementById('stick-me').clientHeight;
+    sticker_ph_top.style.display = 'block'; // puts in a placeholder for where sticky used to be for smooth scrolling
+    sticker_ph.style.display = 'none';
+    sticker.classList.remove('fixed-class');
+    sticker.classList.add('absolute-class');
   }
-  if (i < 5 && i > -1) {
+
+  var currentPosition = getPageScroll()-document.getElementById('stick-here').offsetTop;//-document.getElementById('floor-image').clientHeight;
+  i = Math.floor(currentPosition/1500*4);
+  console.log(i);
+  if (i == 0) {
+    updateImg(0,testImg);
+  }
+  if (i < 4 && i > 0) {
     var data = floor1Data[i-1];
     updateInfo(data);
+    updateImg(i-1,testImg);
+    document.querySelector("#intro").innerHTML = "";
+  } else if (i <= -1) {
+    document.querySelector("#interactive-photo").innerHTML = "";
+    document.querySelector("#interactive-desc").innerHTML = "";
+    document.querySelector("#intro").innerHTML = "The third level of the structure will hold 37 bus bays around an elongated central island where riders will wait, board and depart. The main tenant will be AC Transit, but some of the bays will be used by other buses crossing the bay, including Muni’s Treasure Island service. Unlike the gloomy 1939 terminal, this waiting area will be naturally lit and ventilated, thanks to perforated metal panels that surround it.";
   }
+  // if (i >= 4) {
+  //   document.getElementById('scroll-height').style.height = "500px";
+  // } else {
+  //   document.getElementById('scroll-height').style.height = "1500px";
+  // }
+
 }
-
-
-
-
-
-
-// function resizeImg (img){
-//
-//   console.log(img);
-//
-//   var resize = 150; // resize amount in percentage
-//   var origH  = 61;  // original image height
-//   var origW  = 250; // original image width
-//   var mouseX = event.x;
-//   var mouseY = event.y;
-//   var newH   = origH * (resize / 100);
-//   var newW   = origW * (resize / 100);
-//
-//   console.log(newH);
-//   console.log(newW);
-//
-//   // Set the new width and height
-//   img.style.width  = "120%";
-//
-//   console.log(img.style.width);
-//
-//   var c = img.parentNode;
-//
-//   // Work out the new center
-//   c.scrollLeft = (mouseX * (resize / 100)) - (newW / 2) / 2;
-//   c.scrollTop  = (mouseY * (resize / 100)) - (newH / 2) / 2;
-// }
-//
-// var testDiv = document.getElementById("Test");
-// var testImg = testDiv.getElementsByTagName('img')[0];
-//
-// console.log(testDiv);
-// console.log(testImg);
-//
-// testDiv.addEventListener('click', function() {
-//   resizeImg(testImg);
-// }, false);
