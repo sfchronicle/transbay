f8fde43997a3eb703baa2b6252b44e73e82bf005
emma <emmaruthoneill@gmail.com>
2017-08-28 15:53:49 -0700

    cleaned up some timeline details

--- src/js/part1.js
+++ src/js/part1.js
@@ -3,16 +3,32 @@ require("./lib/social"); //Do not delete
 // timeline event listeners ----------------------------------------------------------------
 
 var timelineOpen = 0;
+var timeline_top;
+var timeline_ticker;
+var timeline_bottom;
+var timeline_close;
 
 document.getElementById("timeline-open").addEventListener("click",function() {
   document.getElementById("timeline-wrapper").classList.add("view");
   timelineOpen = 1;
+  if (timeline_ticker) {
+    timeline_ticker.classList.remove('sticky');
+    timeline_ticker.style.display = "none";
+    timeline_close.classList.remove('sticky');
+    timeline_close.style.display = "none";
+  }
 },false);
 
 document.getElementById("close-timeline-button").addEventListener("click",function() {
   document.getElementById("timeline-wrapper").classList.remove("view");
   timelineOpen = 0;
-  document.getElementById('timeline-top-border').scrollIntoView();
+  // document.getElementById('timeline-top-border').scrollIntoView();
+  var top = $('#timeline-top-border').offset().top;
+  $('html,body').animate({scrollTop: top}, 1000);
+  timeline_ticker.classList.remove('sticky');
+  timeline_ticker.style.display = "none";
+  timeline_close.classList.remove('sticky');
+  timeline_close.style.display = "none";
 },false);
 
 
@@ -115,38 +131,26 @@ function activate() {
   // scrolling commands for timeline ------------------------------------------------------------------
 
   if (timelineOpen == 1) {
-    var timeline_top = document.getElementById('stick-timeline').getBoundingClientRect().top + window_top;
-    var timeline_ticker = document.getElementById('ticker');
-    var timeline_bottom = document.getElementById('e18').getBoundingClientRect().bottom + window_top - 30;
-    var timeline_close = document.getElementById('close-timeline-button');
-
-  //  var timeline_ph = document.getElementById('timeline-placeholder');
+    timeline_top = document.getElementById('stick-timeline').getBoundingClientRect().top + window_top-100;
+    timeline_ticker = document.getElementById('ticker');
+    timeline_bottom = document.getElementById('e18').getBoundingClientRect().bottom + window_top - 30;
+    timeline_close = document.getElementById('close-timeline-button');
 
     if ((window_top > timeline_top) && (window_top < timeline_bottom)) {
-
-        timeline_ticker.style.display = "block";
-        timeline_ticker.classList.add('sticky');
-
-        timeline_close.style.display = "block";
-        timeline_close.classList.add('sticky');
-
+      timeline_ticker.style.display = "block";
+      timeline_ticker.classList.add('sticky');
+      timeline_close.style.display = "block";
+      timeline_close.classList.add('sticky');
     } else {
-        timeline_ticker.classList.remove('sticky');
-        timeline_ticker.style.display = "none";
-
-        timeline_close.classList.remove('sticky');
-        timeline_close.style.display = "none";
-
+      timeline_ticker.classList.remove('sticky');
+      timeline_ticker.style.display = "none";
+      timeline_close.classList.remove('sticky');
+      timeline_close.style.display = "none";
     }
-
-    console.log(timelineData);
 
     for (var i = 0; i < timelineData.length; i++ ) {
     	var a = document.getElementById("e"+i);
     	var at = document.getElementById("t-"+i);
-
-      console.log(a);
-      console.log(at);
 
       var ed_top = a.getBoundingClientRect().top + window_top - 100;
   	  var ede_top = a.getBoundingClientRect().bottom + window_top - 100;
