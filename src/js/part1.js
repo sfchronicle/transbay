ce4a6529836c3698d723fc7befe67ba138fbabd6
emma <emmaruthoneill@gmail.com>
2017-09-05 19:10:15 -0700

    think i just fixed an important bug

--- src/js/part1.js
+++ src/js/part1.js
@@ -3,6 +3,95 @@ require("./lib/social"); //Do not delete
 // require("d3.chart");
 // var Sankey = require('./lib/d3.chart.sankey');
 
+
+// -----------------------------------------------------------------------------
+// ANIMATIONS variables
+// -----------------------------------------------------------------------------
+
+var timeoutTime = 500;
+var fadeTime = 300;
+var timer;
+
+// -----------------------------------------------------------------------------
+// PARK animations --------------------------------------------------
+// -----------------------------------------------------------------------------
+
+var parkFloor = $("#overlay-floor0");
+var iPark = -1;
+var park_urls = ["transitcenter_PARKARROWS.png","transitcenter_PARKTEXTNOARROWS.png"].map(s => "../assets/graphics/" + s);
+
+//callback
+var swap_park = function() {
+  parkFloor.fadeOut(fadeTime, function() {
+    iPark = (iPark + 1) % park_urls.length; // hello modulo, my old friend
+    parkFloor.attr("src", park_urls[iPark]);
+    parkFloor.one("load", function() {
+      parkFloor.fadeIn(fadeTime, () => setTimeout(swap_park, timeoutTime));
+    });
+  })
+};
+
+// -----------------------------------------------------------------------------
+// BUS animations --------------------------------------------------
+// -----------------------------------------------------------------------------
+
+var busFloor = $("#overlay-floor1");
+var iBus = -1;
+var bus_urls = ["transitcenter_BUSARROWS3.png","transitcenter_BUSARROWS2.png","transitcenter_BUSARROWS1.png"].map(s => "../assets/graphics/" + s);
+
+//callback
+var swap_bus = function() {
+  busFloor.fadeOut(fadeTime, function() {
+    iBus = (iBus + 1) % bus_urls.length; // hello modulo, my old friend
+    busFloor.attr("src", bus_urls[iBus]);
+    busFloor.attr("z-index","100");
+    busFloor.one("load", function() {
+      busFloor.fadeIn(fadeTime, () => setTimeout(swap_bus, timeoutTime));
+    });
+  })
+};
+
+// -----------------------------------------------------------------------------
+// MEZZANINE animations --------------------------------------------------
+// -----------------------------------------------------------------------------
+
+var mezFloor = $("#overlay-floor2");
+var iMez = -1;
+var mez_urls = ["transitcenter_MEZZANINEARROWS.png","transitcenter_MEZZANINENOARROWS.png"].map(s => "../assets/graphics/" + s);
+
+//callback
+var swap_mez = function() {
+  mezFloor.fadeOut(fadeTime, function() {
+    iMez = (iMez + 1) % mez_urls.length; // hello modulo, my old friend
+    mezFloor.attr("src", mez_urls[iMez]);
+    mezFloor.attr("z-index","100");
+    mezFloor.one("load", function() {
+      mezFloor.fadeIn(fadeTime, () => setTimeout(swap_mez, timeoutTime));
+    });
+  })
+};
+
+// -----------------------------------------------------------------------------
+// GROUND animations --------------------------------------------------
+// -----------------------------------------------------------------------------
+
+var groundFloor = $("#overlay-floor3");
+var iGround = -1;
+var ground_urls = ["transitcenter_GROUNDARROWS.png","transitcenter_GROUNDNOARROWS.png"].map(s => "../assets/graphics/" + s);
+
+//callback
+var swap_ground = function() {
+  groundFloor.fadeOut(fadeTime, function() {
+    iGround = (iGround + 1) % ground_urls.length; // hello modulo, my old friend
+    groundFloor.attr("src", ground_urls[iGround]);
+    groundFloor.attr("z-index","100");
+    groundFloor.one("load", function() {
+      groundFloor.fadeIn(fadeTime, () => setTimeout(swap_ground, timeoutTime));
+    });
+  })
+};
+
+
 // timeline event listeners ----------------------------------------------------------------
 
 var timelineOpen = 0;
@@ -60,20 +149,31 @@ if (screen.width <= 480) {
   var zoomMult = 0.9;
   var zoomOffset = 450;
 } else {
-  var floorZoom = "130%";
+  var floorZoom = "125%";
   var zoomMult = 0.3;
   var zoomOffset = 250;
 }
+
+var lastScrollTop = document.body.scrollTop;
+var direction;
 
 function activate() {
 
   // general scrolling variable ----------------------------
   var window_top = document.body.scrollTop;//getPageScroll();//
+  if (window_top > lastScrollTop) {
+    direction = "down";
+  } else {
+    direction = "up";
+  }
+  lastScrollTop = window_top;
+  console.log(direction);
 
   // scrolling commands for interactive graphic ----------------
   var sticker_start = document.getElementById('stick-here').getBoundingClientRect().top + window_top-37;
-  var sticker_stop = document.getElementById('stop-stick-here').getBoundingClientRect().top + window_top;// - placeholderHeight;
+  var sticker_stop = document.getElementById('stop-stick-here').getBoundingClientRect().top + window_top;
 
+  // loop through the floors to see which one we are in and
   for (var s = 0; s < floorplanData.stages.length; s++ ) {
     var sticker_ph = document.getElementById('stick-ph'+s);
     var showf = document.getElementById("showfloor"+s);
@@ -84,17 +184,17 @@ function activate() {
     var placeholderWidth = document.getElementById('floor'+s).clientWidth;
 
     var f_top = showf.getBoundingClientRect().top + window_top - 37;
-    if (s < (floorplanData.stages.length-1)){
-      var f_bottom = showf.getBoundingClientRect().bottom + window_top;// + 100;
-    } else {
-      var f_bottom = showf.getBoundingClientRect().bottom + window_top;// - 500;
-    }
+    // if (s < (floorplanData.stages.length-1)){
+    //   var f_bottom = showf.getBoundingClientRect().bottom + window_top;// + 100;
+    // } else {
+    var f_bottom = showf.getBoundingClientRect().bottom + window_top;// - 500;
+    // }
     var zoomf_top = zoomf.getBoundingClientRect().top + window_top - 37 - zoomOffset;
     var floorImg = document.getElementById("background-floor"+s);
     var overlayDiv = document.getElementById("overlay-floor"+s);
 
-    // we are in the interactive, above the bottom and below the top
-    if (window_top > f_top && window_top < f_bottom) {
+    // we are in a section of the interactive, above the bottom and below the top
+    if (window_top >= f_top && window_top <= f_bottom) {
 
       console.log("we are in the interactive");
       f.classList.add('fixed');
@@ -102,38 +202,75 @@ function activate() {
       sticker_ph.style.display = 'block';
       IDXprev = s;
 
-      if ((window_top+350) > f_bottom){
-        console.log("at second place");
+      // at the bottom of a section, un-zoom the image but keep it visible and sticky
+      if ((window_top + 350) > f_bottom){
+        console.log("at the bottom of a section, unzooming but keeping it visible and sticky");
         floorImg.style.opacity = "1";
-        overlayDiv.style.opacity = "1";
+        floorImg.style.visibility = "visible";
         floorImg.style.width  = "100%";
         floorImg.style.marginLeft = "0px";
-        overlayDiv.style.marginLeft = "0px";
-        overlayDiv.style.visibility  = "visible";
-      } else if (window_top > zoomf_top){
-        console.log("at third place");
-        floorImg.style.opacity = "1";
+
         overlayDiv.style.opacity = "1";
+        overlayDiv.style.marginLeft = "0px";
+
+      // going through the images part of a section: zoom & pan image
+      } else if (window_top > zoomf_top){
+        console.log("in the middle of a section, zooming and panning");
+
+        // show the background image and zoom it
+        floorImg.style.opacity = "1";
         floorImg.style.width = floorZoom;
+
+        // hide the overlay
+        overlayDiv.style.opacity = "1";
         overlayDiv.style.visibility  = "hidden";
-        var scrollLeft = (Math.round((f_bottom-window_top)/(f_bottom-zoomf_top)*placeholderWidth)-placeholderWidth)*zoomMult;
+
+        // setting the panning on the image
+        if (direction == "up") {
+          var scrollLeft = (Math.round(placeholderWidth-(f_bottom-window_top)/(f_bottom-zoomf_top)*placeholderWidth))*zoomMult;
+        } else {
+          var scrollLeft = (Math.round((f_bottom-window_top)/(f_bottom-zoomf_top)*placeholderWidth)-placeholderWidth)*zoomMult;
+        }
         console.log(scrollLeft);
         floorImg.style.marginLeft = scrollLeft+ 'px';
         overlayDiv.style.marginLeft = scrollLeft+ 'px';
+
+      // at the top of the image, showing the overlays and no zoom and no pan
       } else {
-        console.log("at fourth place");
+        console.log("at top of section, showing overlays, no zoom, no pan");
+
+        // activate correct overlays
+        // if (s == 0 && !(swapPark)) {
+        //   console.log("loop park overlays");
+        //   // clearTimeout(swap_park);
+        //   setTimeout(swap_park, timeoutTime);
+        //   var swapPark = 1;
+        // } else if (s == 1 && !swapBus) {
+        //   console.log("loop bus overlays");
+        //   setTimeout(swap_bus, timeoutTime);
+        //   var swapBus = 1;
+        // } else if (s == 2 && !swapMez) {
+        //   console.log("loop mezzanine overlays");
+        //   setTimeout(swap_mez, timeoutTime);
+        //   var swapMez = 1;
+        // } else if (s == 3 && !swapGround) {
+        //   console.log("loop ground overlays");
+        //   setTimeout(swap_ground, timeoutTime);
+        //   var swapGround = 1;
+        // }
+
         floorImg.style.opacity = "1";
-        overlayDiv.style.opacity = "1";
         floorImg.style.width  = "100%";
         floorImg.style.marginLeft = "0px";
+
+        overlayDiv.style.opacity = "1";
         overlayDiv.style.width  = "100%";
         overlayDiv.style.marginLeft = "0px";
         overlayDiv.style.visibility  = "visible";
       }
 
     // we are not in the interactive
-    // if the
-    } else if ((IDXprev == s) && (IDXprev != -1)){
+    } else {
       floorImg.style.opacity = "0";
       overlayDiv.style.opacity = "0";
     }
@@ -151,8 +288,16 @@ function activate() {
     if (window_top < sticker_start) {
       floorImg.style.opacity = "1";
     }
-    // showing the image at the bottom doesn't work because the relative position is above where you would see it as a reader
+    // NOTE: showing the image at the bottom doesn't work because the relative position is above where you would see it as a reader
   };
+  console.log(window_top);
+  console.log(sticker_stop);
+  if (window_top > sticker_stop-document.getElementById('floor3').clientHeight) {
+    console.log("hiding the bottom image");
+    document.getElementById("background-floor3").style.visibility = "hidden";
+  } else {
+    document.getElementById("background-floor3").style.visibility = "visible";
+  }
 
   // scrolling commands for timeline ------------------------------------------------------------------
 
@@ -192,96 +337,6 @@ function activate() {
   }
 
 }
-
-// -----------------------------------------------------------------------------
-// ANIMATIONS variables
-// -----------------------------------------------------------------------------
-
-var timeoutTime = 500;
-var fadeTime = 100;
-
-// -----------------------------------------------------------------------------
-// PARK animations --------------------------------------------------
-// -----------------------------------------------------------------------------
-
-var parkFloor = $("#overlay-floor0");
-var iPark = -1;
-var park_urls = ["transitcenter_PARKARROWS.png","transitcenter_PARKTEXTNOARROWS.png"].map(s => "../assets/graphics/" + s);
-
-//callback
-var swap_park = function() {
-  parkFloor.fadeOut(fadeTime, function() {
-    iPark = (iPark + 1) % park_urls.length; // hello modulo, my old friend
-    parkFloor.attr("src", park_urls[iPark]);
-    parkFloor.one("load", function() {
-      parkFloor.fadeIn(fadeTime, () => setTimeout(swap_park, timeoutTime));
-    });
-  })
-};
-setTimeout(swap_park, timeoutTime);
-
-// -----------------------------------------------------------------------------
-// BUS animations --------------------------------------------------
-// -----------------------------------------------------------------------------
-
-var busFloor = $("#overlay-floor1");
-var iBus = -1;
-var bus_urls = ["transitcenter_BUSARROWS3.png","transitcenter_BUSARROWS2.png","transitcenter_BUSARROWS1.png"].map(s => "../assets/graphics/" + s);
-
-//callback
-var swap_bus = function() {
-  busFloor.fadeOut(fadeTime, function() {
-    iBus = (iBus + 1) % bus_urls.length; // hello modulo, my old friend
-    busFloor.attr("src", bus_urls[iBus]);
-    busFloor.attr("z-index","100");
-    busFloor.one("load", function() {
-      busFloor.fadeIn(fadeTime, () => setTimeout(swap_bus, timeoutTime));
-    });
-  })
-};
-setTimeout(swap_bus, timeoutTime);
-
-// -----------------------------------------------------------------------------
-// MEZZANINE animations --------------------------------------------------
-// -----------------------------------------------------------------------------
-
-var mezFloor = $("#overlay-floor2");
-var iMez = -1;
-var mez_urls = ["transitcenter_MEZZANINEARROWS.png","transitcenter_MEZZANINENOARROWS.png"].map(s => "../assets/graphics/" + s);
-
-//callback
-var swap_mez = function() {
-  mezFloor.fadeOut(fadeTime, function() {
-    iMez = (iMez + 1) % mez_urls.length; // hello modulo, my old friend
-    mezFloor.attr("src", mez_urls[iMez]);
-    mezFloor.attr("z-index","100");
-    mezFloor.one("load", function() {
-      mezFloor.fadeIn(fadeTime, () => setTimeout(swap_mez, timeoutTime));
-    });
-  })
-};
-setTimeout(swap_mez, timeoutTime);
-
-// -----------------------------------------------------------------------------
-// GROUND animations --------------------------------------------------
-// -----------------------------------------------------------------------------
-
-var groundFloor = $("#overlay-floor3");
-var iGround = -1;
-var ground_urls = ["transitcenter_GROUNDARROWS.png","transitcenter_GROUNDNOARROWS.png"].map(s => "../assets/graphics/" + s);
-
-//callback
-var swap_ground = function() {
-  groundFloor.fadeOut(fadeTime, function() {
-    iGround = (iGround + 1) % ground_urls.length; // hello modulo, my old friend
-    groundFloor.attr("src", ground_urls[iGround]);
-    groundFloor.attr("z-index","100");
-    groundFloor.one("load", function() {
-      groundFloor.fadeIn(fadeTime, () => setTimeout(swap_ground, timeoutTime));
-    });
-  })
-};
-setTimeout(swap_ground, timeoutTime);
 
 
 // flow chart for financial data ---------------------------------------------------
