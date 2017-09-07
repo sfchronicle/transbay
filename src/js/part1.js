67146c6e5dc4c66f950bd083e13a4b4af0614d7b
emma <emmaruthoneill@gmail.com>
2017-09-07 11:13:41 -0700

    commented the js a bunch

--- src/js/part1.js
+++ src/js/part1.js
@@ -100,6 +100,7 @@ var timeline_ticker;
 var timeline_bottom;
 var timeline_close;
 
+// show the timeline on button click
 document.getElementById("timeline-open").addEventListener("click",function() {
   document.getElementById("timeline-wrapper").classList.add("view");
   timelineOpen = 1;
@@ -111,6 +112,7 @@ document.getElementById("timeline-open").addEventListener("click",function() {
   }
 },false);
 
+// hide the timeline on button click
 document.getElementById("close-timeline-button").addEventListener("click",function() {
   document.getElementById("timeline-wrapper").classList.remove("view");
   timelineOpen = 0;
@@ -124,12 +126,13 @@ document.getElementById("close-timeline-button").addEventListener("click",functi
 },false);
 
 
-// -----------------------------------------------------------------------------------------
+// on scroll, run the activate function ----------------------------------------
 
 window.onscroll = function() {activate()};
 
 // desktop scrolling controls ----------------------------------------------------------------
 
+// we are not using this but I'm keeping it for now in case I need it
 function getPageScroll() {
   var yScroll;
 
@@ -143,12 +146,17 @@ function getPageScroll() {
   return yScroll;
 }
 
+// setting scrolly interactive variables
+
 var IDXprev = -1;
+
 if (screen.width <= 480) {
+  // we are zooming on mobile
   var floorZoom = "160%";
   var zoomMult = 1.6;
   var zoomOffset = 450;
 } else {
+  // we are not zooming on desktop
   var floorZoom = "100%";
   var zoomMult = 1;
   var zoomOffset = 250;
@@ -160,9 +168,10 @@ var swapPark = 0, swapBus = 0, swapMez = 0, swapGround = 0;
 var circle = document.getElementById("highlightCircle");
 var circleFlag, circleTop, circleLeft;
 
+// this is the monster scrolling function
 function activate() {
 
-  // general scrolling variable ----------------------------
+  // general scrolling position and direction ----------------------------
   var window_top = document.body.scrollTop;//getPageScroll();//
   if (window_top > lastScrollTop) {
     direction = "down";
@@ -175,22 +184,39 @@ function activate() {
   var sticker_start = document.getElementById('stick-here').getBoundingClientRect().top + window_top-37;
   var sticker_stop = document.getElementById('stop-stick-here').getBoundingClientRect().top + window_top;
 
-  // loop through the floors to see which one we are in and
-  circleFlag = 0;
-  for (var s = 0; s < floorplanData.stages.length; s++ ) {
-    var sticker_ph = document.getElementById('stick-ph'+s);
-    var showf = document.getElementById("showfloor"+s);
-    var f = document.getElementById("floor"+s);
-    var zoomf = document.getElementById("zoom-floor"+s);
+  // loop through the floors to see which one we are in and display appropriate layers etc
 
+  // initialize circle highlight to be hidden, in case we're not in the image section
+  circleFlag = 0;
+
+  // starting the loop
+  for (var s = 0; s < floorplanData.stages.length; s++ ) {
+
+    // placeholder for each section for when we take top image out of the page
+    var sticker_ph = document.getElementById('stick-ph'+s);
+
+    // how big the placeholder is (the top image)
     var placeholderHeight = document.getElementById('floor'+s).clientHeight;
     var placeholderWidth = document.getElementById('floor'+s).clientWidth;
 
+    // top of section
+    var showf = document.getElementById("showfloor"+s);
     var f_top = showf.getBoundingClientRect().top + window_top - 37;
     var f_bottom = showf.getBoundingClientRect().bottom + window_top;
 
+    // fixed image at top of section
+    var f = document.getElementById("floor"+s);
+
+    // start of zooming part of section
+    var zoomf = document.getElementById("zoom-floor"+s);
+
+    // where to start zooming
     var zoomf_top = zoomf.getBoundingClientRect().top + window_top - 37 - zoomOffset;
+
+    // image that we're going to fix at the top
     var floorImg = document.getElementById("background-floor"+s);
+
+    // overlay image that we are going to swap out to show arrows
     var overlayDiv = document.getElementById("overlay-floor"+s);
 
     // we are in a section of the interactive, above the bottom and below the top
@@ -228,26 +254,40 @@ function activate() {
         // show the highlight circle
         circleFlag = 1;
 
+        // on mobile, we want to zoom & pan the image and move the highlight up and down a little
         if (screen.width <= 480) {
 
-          // was thinking about using this as a variable for zooming but am not right now
+          // was thinking about using this as a variable but not actually using it
           var panDiff = (zoomMult-1)*screen.width;
 
+          // which image are we showing in the image sequence
           var testIDX = Math.min(Math.max(Math.floor((window_top - zoomf_top)/1000),0),floorplanData["stages"][s]["images"].length-1);
+
+          // finding the left percent for that image
           var leftNum = +floorplanData["stages"][s]["images"][testIDX]["LeftPercent"]*zoomMult;
+
+          // the feature is on the left side of the image
           if (leftNum <= 0.5) {
             console.log("feature is on left");
             var scrollLeft = (0.5 - leftNum)*placeholderWidth-20;//-20/zoomMult;
             circleTop = floorplanData["stages"][s]["images"][testIDX]["TopPercent"]*placeholderHeight+5*zoomMult;//*zoomMult;//*zoomMult;
+
+          // the feature is on the right side of the image
           } else {
             console.log("feature is on right");
             var scrollLeft = -(leftNum - 0.5)*placeholderWidth-30;
             circleTop = floorplanData["stages"][s]["images"][testIDX]["TopPercent"]*placeholderHeight+5*zoomMult;//*zoomMult;//*zoomMult;//*zoomMult;
           }
+
+          // apply the panning to the image and overlay
           floorImg.style.marginLeft = scrollLeft+ 'px';
           overlayDiv.style.marginLeft = scrollLeft+ 'px';
+
         } else {
+          // find the image that we are showing in the image sequence
           var testIDX = Math.min(Math.max(Math.floor((window_top - zoomf_top)/600),0),floorplanData["stages"][s]["images"].length-1);
+
+          // compute how to move the circle to center on the feature
           circleTop = floorplanData["stages"][s]["images"][testIDX]["TopPercent"]*placeholderHeight;
           circleLeft = floorplanData["stages"][s]["images"][testIDX]["LeftPercent"]*placeholderWidth;
         }
@@ -275,10 +315,12 @@ function activate() {
           swapGround = 1;
         }
 
+        // showing the main image and unzooming and unpanning it
         floorImg.style.opacity = "1";
         floorImg.style.width  = "100%";
         floorImg.style.marginLeft = "0px";
 
+        // showing the overlay and unzooming and unpanning it
         overlayDiv.style.opacity = "1";
         overlayDiv.style.width  = "100%";
         overlayDiv.style.marginLeft = "0px";
@@ -287,6 +329,7 @@ function activate() {
 
     // we are not in the interactive
     } else {
+      // do not want to show the images
       floorImg.style.opacity = "0";
       overlayDiv.style.opacity = "0";
     }
@@ -332,15 +375,6 @@ function activate() {
     // we are hiding the highlight
     circle.style.opacity = "0";
   }
-
-
-  // THIS IS NOT QUITE WORKING
-  // if (window_top > sticker_stop-document.getElementById('floor3').clientHeight) {
-  //   console.log("hiding the bottom image");
-  //   document.getElementById("background-floor3").style.visibility = "hidden";
-  // } else {
-  //   document.getElementById("background-floor3").style.visibility = "visible";
-  // }
 
   // scrolling commands for timeline ------------------------------------------------------------------
 
