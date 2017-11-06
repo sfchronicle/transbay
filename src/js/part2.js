c710aea4d783ffe956a26cac10f469d6884360d9
Emma O'Neill <emmaruthoneill@gmail.com>
2017-11-06 14:02:35 -0800

    styles updated

--- src/js/part2.js
+++ src/js/part2.js
@@ -1,12 +1,10 @@
-console.log(interactiveData);
-
 $(window).scroll(function(){
 
     var pos = $(this).scrollTop();
     var top_pos = $('#sticky-map-top').offset().top-100;
+    var bottom_pos = $('#sticky-map-bottom').offset().top;
     var mapHeight = $("#scroll-png").height();
 
-    // var inc_list = [142, 291, 465, 565, 660, 797, 913, 973, 1203];
     var inc = mapHeight/5; //how often we should see new map element
     if(pos >= top_pos) {
         $('.floor-info').css("display","none");
@@ -15,17 +13,14 @@ $(window).scroll(function(){
       $('.floor-info').css("display","block");
       var idx = Math.round((pos-top_pos)/inc);
       if (idx < 5 && idx >= 0) {
-        var inc_new = inc*idx;
+        var inc_new = inc*idx+20;
         var top_padding = inc_new+"px";
-        // if (interactiveData[idx].Image) {
-        //   var html_str = "<div class='title'>"+interactiveData[idx].Title+ "</div><div class='deck'>"+interactiveData[idx].Deck+"<div class='inset-img'><img src='"+interactiveData[idx].Image+"'></div>";
-        // } else {
-        //   var html_str = "<div class='title'>"+interactiveData[idx].Title+ "</div><div class='deck'>"+interactiveData[idx].Deck+"</div>";
-        // }
         $(".floor").css("display","none")
         $("#floor"+idx).css("display","block")
-        // $('.floor-info').html(html_str);
         $('.floor-info').css('top',top_padding);
       }
+    }
+    if (pos > bottom_pos) {
+      $(".floor").css("display","none")
     }
   });
