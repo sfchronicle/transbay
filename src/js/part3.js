8ae159e1e920a7f69beaaeed1dda574f9165186d
Emma O'Neill <emmaruthoneill@gmail.com>
2017-12-13 11:53:05 -0800

    got panorama photos working

---
+++ src/js/part3.js
@@ -0,0 +1,34 @@
+require("./lib/social"); //Do not delete
+
+var viewer = PhotoSphereViewer({
+  container: 'panorama1',
+  panorama: '../assets/photos/part3/1406.jpg',
+  navbar: [
+    'autorotate',
+    'zoom',
+    'caption',
+    'fullscreen'
+  ]
+});
+
+var viewer = PhotoSphereViewer({
+  container: 'panorama2',
+  panorama: '../assets/photos/part3/1407.jpg',
+  navbar: [
+    'autorotate',
+    'zoom',
+    'caption',
+    'fullscreen'
+  ]
+});
+
+var viewer = PhotoSphereViewer({
+  container: 'panorama3',
+  panorama: '../assets/photos/part3/1408.jpg',
+  navbar: [
+    'autorotate',
+    'zoom',
+    'caption',
+    'fullscreen'
+  ]
+});
