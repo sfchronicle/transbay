bb9399529954faf929421db90013e20bac58dcdc
Emma O'Neill <emmaruthoneill@gmail.com>
2017-12-14 14:45:09 -0800

    adding a lot of files

--- src/js/part3.js
+++ src/js/part3.js
@@ -15,19 +15,14 @@ audio_info.forEach(function(d,dIDX){
   });
 });
 
-
-$("#SalesforceTower").on("click",function(){
-  console.log("CLICK");
-});
-
-console.log(flyover_info);
-
 flyover_info.forEach(function(d,dIDX){
   console.log(d.Id);
   if (d.Id){
     document.getElementById(d.Id).addEventListener("mouseenter", function( event ) {
-      event.target.addClass("highlight");
-      console.log("HERE");
+      document.getElementById("flyover-text").innerHTML = "<div class='name'>"+d.Title+"</div><div class='address'>"+d.Address+"</div><div class='description'>"+d.Description+"</div><div class='timeline'>"+d.Timeline+"</div>";
+    });
+    document.getElementById(d.Id).addEventListener("mouseout", function( event ) {
+      document.getElementById("flyover-text").innerHTML = "";
     });
   };
 });
