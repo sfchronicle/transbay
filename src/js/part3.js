1362229a00f4b8e073ba412fa06ba05e4d20e67a
Emma O'Neill <emmaruthoneill@gmail.com>
2017-12-15 14:04:01 -0800

    i love writing javascript

--- src/js/part3.js
+++ src/js/part3.js
@@ -11,7 +11,9 @@ audio_info.forEach(function(d,dIDX){
       'zoom',
       'caption',
       'fullscreen'
-    ]
+    ],
+    default_long: +d.Lon,//d.Lon
+    default_lat: 0//d.Lat,
   });
 });
 
@@ -36,14 +38,73 @@ $(window).scroll(function(){
   if(pos <= top_pos) {
     $('.audio-placeholder').css("display","none");
     $('#audio-map').removeClass("fixedmap");
+    audio_info.forEach(function(p,pdx){
+      var num = pdx+1;
+      drawLine("#PATH"+num,0)
+    });
   }
   if(pos > top_pos) {
     $('.audio-placeholder').css("display","block");
     $('#audio-map').addClass("fixedmap");
+    // check to see where you are along paths
+    audio_info.forEach(function(p,pdx){
+      var SectionStart = $('#audiotour'+pdx).offset().top-100;
+      if (pdx<audio_info.length-1){
+        var SectionEnd = $('#audiotour'+(pdx+1)).offset().top-100;
+      } else {
+        var SectionEnd = bottom_pos;
+      }
+      if (pos > SectionStart && pos < SectionEnd) {
+        var scrollPercentage = 1-(SectionEnd-pos)/(SectionEnd-SectionStart);
+        var num = pdx+1;
+        drawLine("#PATH"+num,scrollPercentage)
+      }
+    });
   }
   if (pos > bottom_pos) {
     $('#audio-map').removeClass("fixedmap");
     $('.audio-placeholder').css("display","none");
+    audio_info.forEach(function(p,pdx){
+      var num = pdx+1;
+      drawLine("#PATH"+num,1)
+    });
   }
 
 });
+
+
+function drawLine(pathName,scrollPercentage){
+  // Get a reference to the <path>
+  console.log(pathName);
+  console.log(document.querySelector(pathName));
+  var path = document.querySelector(pathName).getElementsByTagName("polyline")[0];
+  console.log(path);
+  path.style["stroke-width"] = "4 !important";
+  // Get length of path... ~577px in this case
+  var pathLength = path.getTotalLength();
+
+  // Make very long dashes (the length of the path itself)
+  path.style.strokeDasharray = pathLength + ' ' + pathLength;
+
+  // Offset the dashes so the it appears hidden entirely
+  path.style.strokeDashoffset = pathLength;
+
+  // Jake Archibald says so
+  // https://jakearchibald.com/2013/animated-line-drawing-svg/
+  path.getBoundingClientRect();
+
+  // Length to offset the dashes
+  var drawLength = pathLength * scrollPercentage;
+
+  // Draw in reverse
+  path.style.strokeDashoffset = pathLength - drawLength;
+
+  // When complete, remove the dash array, otherwise shape isn't quite sharp
+ // Accounts for fuzzy math
+  if (scrollPercentage >= 0.99) {
+    path.style.strokeDasharray = "none";
+
+  } else {
+    path.style.strokeDasharray = pathLength + ' ' + pathLength;
+  }
+}
